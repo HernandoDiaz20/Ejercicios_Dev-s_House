01546dfd4705df9759d547a58f3323fd382a9166
HernandoDiaz20 <diazgonzalezhernando22@gmail.com>
2024-04-13 17:53:42 -0500

    Subiendo Proyecto

---
+++ App_Cap11_al_34/App_Cap11_al_34/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace App_Cap11_al_34
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSumaLista = new System.Windows.Forms.TextBox();
+            this.txtSumaArray = new System.Windows.Forms.TextBox();
+            this.txtSumaMatriz = new System.Windows.Forms.TextBox();
+            this.txtSumaVector = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.listNumLista = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listNumArrayList = new System.Windows.Forms.ListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listNumVector = new System.Windows.Forms.ListBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.listNumMatriz = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtSumaLista
+            // 
+            this.txtSumaLista.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtSumaLista.Font = new System.Drawing.Font("Elephant", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSumaLista.Location = new System.Drawing.Point(77, 99);
+            this.txtSumaLista.Name = "txtSumaLista";
+            this.txtSumaLista.ReadOnly = true;
+            this.txtSumaLista.Size = new System.Drawing.Size(100, 23);
+            this.txtSumaLista.TabIndex = 0;
+            this.txtSumaLista.TabStop = false;
+            // 
+            // txtSumaArray
+            // 
+            this.txtSumaArray.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtSumaArray.Font = new System.Drawing.Font("Elephant", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSumaArray.Location = new System.Drawing.Point(260, 99);
+            this.txtSumaArray.Name = "txtSumaArray";
+            this.txtSumaArray.ReadOnly = true;
+            this.txtSumaArray.Size = new System.Drawing.Size(100, 23);
+            this.txtSumaArray.TabIndex = 1;
+            this.txtSumaArray.TabStop = false;
+            // 
+            // txtSumaMatriz
+            // 
+            this.txtSumaMatriz.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtSumaMatriz.Font = new System.Drawing.Font("Elephant", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSumaMatriz.Location = new System.Drawing.Point(260, 241);
+            this.txtSumaMatriz.Name = "txtSumaMatriz";
+            this.txtSumaMatriz.ReadOnly = true;
+            this.txtSumaMatriz.Size = new System.Drawing.Size(100, 23);
+            this.txtSumaMatriz.TabIndex = 2;
+            this.txtSumaMatriz.TabStop = false;
+            // 
+            // txtSumaVector
+            // 
+            this.txtSumaVector.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtSumaVector.Font = new System.Drawing.Font("Elephant", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSumaVector.Location = new System.Drawing.Point(77, 241);
+            this.txtSumaVector.Name = "txtSumaVector";
+            this.txtSumaVector.ReadOnly = true;
+            this.txtSumaVector.Size = new System.Drawing.Size(100, 23);
+            this.txtSumaVector.TabIndex = 3;
+            this.txtSumaVector.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(69, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Suma de la Lista";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(247, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Suma del ArrayList";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(69, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Suma del Vector";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(247, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 19);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Suma de la Matriz";
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.BackColor = System.Drawing.Color.LawnGreen;
+            this.btnGenerar.Font = new System.Drawing.Font("Lucida Fax", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerar.Location = new System.Drawing.Point(238, 337);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(75, 32);
+            this.btnGenerar.TabIndex = 8;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = false;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.BackColor = System.Drawing.Color.Yellow;
+            this.btnProcesar.Font = new System.Drawing.Font("Lucida Fax", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcesar.Location = new System.Drawing.Point(378, 337);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(75, 32);
+            this.btnProcesar.TabIndex = 9;
+            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = false;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnLimpiar.Font = new System.Drawing.Font("Lucida Fax", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(516, 337);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 32);
+            this.btnLimpiar.TabIndex = 10;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // listNumLista
+            // 
+            this.listNumLista.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.listNumLista.Font = new System.Drawing.Font("Lucida Fax", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listNumLista.FormattingEnabled = true;
+            this.listNumLista.ItemHeight = 15;
+            this.listNumLista.Location = new System.Drawing.Point(436, 71);
+            this.listNumLista.Name = "listNumLista";
+            this.listNumLista.Size = new System.Drawing.Size(120, 79);
+            this.listNumLista.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(426, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(141, 19);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Numeros de la Lista";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(605, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(158, 19);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Numeros del ArrayList";
+            // 
+            // listNumArrayList
+            // 
+            this.listNumArrayList.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.listNumArrayList.Font = new System.Drawing.Font("Lucida Fax", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listNumArrayList.FormattingEnabled = true;
+            this.listNumArrayList.ItemHeight = 15;
+            this.listNumArrayList.Location = new System.Drawing.Point(619, 71);
+            this.listNumArrayList.Name = "listNumArrayList";
+            this.listNumArrayList.Size = new System.Drawing.Size(120, 79);
+            this.listNumArrayList.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(426, 181);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(143, 19);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Numeros del Vector";
+            // 
+            // listNumVector
+            // 
+            this.listNumVector.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.listNumVector.Font = new System.Drawing.Font("Lucida Fax", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listNumVector.FormattingEnabled = true;
+            this.listNumVector.ItemHeight = 15;
+            this.listNumVector.Location = new System.Drawing.Point(436, 214);
+            this.listNumVector.Name = "listNumVector";
+            this.listNumVector.Size = new System.Drawing.Size(120, 79);
+            this.listNumVector.TabIndex = 16;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(605, 181);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(154, 19);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Numeros de la Matriz";
+            // 
+            // listNumMatriz
+            // 
+            this.listNumMatriz.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.listNumMatriz.Font = new System.Drawing.Font("Lucida Fax", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listNumMatriz.FormattingEnabled = true;
+            this.listNumMatriz.ItemHeight = 15;
+            this.listNumMatriz.Location = new System.Drawing.Point(619, 214);
+            this.listNumMatriz.Name = "listNumMatriz";
+            this.listNumMatriz.Size = new System.Drawing.Size(120, 79);
+            this.listNumMatriz.TabIndex = 18;
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(815, 396);
+            this.Controls.Add(this.listNumMatriz);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.listNumVector);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.listNumArrayList);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listNumLista);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnProcesar);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSumaVector);
+            this.Controls.Add(this.txtSumaMatriz);
+            this.Controls.Add(this.txtSumaArray);
+            this.Controls.Add(this.txtSumaLista);
+            this.Name = "FormPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form Principal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSumaLista;
+        private System.Windows.Forms.TextBox txtSumaArray;
+        private System.Windows.Forms.TextBox txtSumaMatriz;
+        private System.Windows.Forms.TextBox txtSumaVector;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.ListBox listNumLista;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox listNumArrayList;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListBox listNumVector;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ListBox listNumMatriz;
+    }
+}
+
---
+++ App_Cap11_al_34/App_Cap11_al_34/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App_Cap11_al_34
+{
+    public partial class FormPrincipal : Form
+    {
+        List<int> listaNumeros = new List<int>();
+        ArrayList arrayNumeros = new ArrayList();
+        int[] vectorNumeros = new int[5];
+        int[,] matrizNumeros = new int[3, 3];
+
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+            // Generar números aleatorios únicos y almacenar en diferentes estructuras de datos
+            while (listaNumeros.Count < 5)
+            {
+                int numero = rnd.Next(1, 101);
+                if (!listaNumeros.Contains(numero))
+                {
+                    listaNumeros.Add(numero);
+                }
+            }
+
+            while (arrayNumeros.Count < 5)
+            {
+                int numero = rnd.Next(1, 101);
+                if (!arrayNumeros.Contains(numero))
+                {
+                    arrayNumeros.Add(numero);
+                }
+            }
+
+            for (int i = 0; i < vectorNumeros.Length; i++)
+            {
+                int numero = rnd.Next(1, 101);
+                while (Array.IndexOf(vectorNumeros, numero) != -1)
+                {
+                    numero = rnd.Next(1, 101);
+                }
+                vectorNumeros[i] = numero;
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    matrizNumeros[i, j] = rnd.Next(1, 101);
+                }
+            }
+
+            // Mostrar los números en los ListBox
+            listNumLista.Items.Clear();
+            listNumArrayList.Items.Clear();
+            listNumVector.Items.Clear();
+            listNumMatriz.Items.Clear();
+
+            foreach (int num in listaNumeros)
+            {
+                listNumLista.Items.Add(num);
+            }
+
+            foreach (int num in arrayNumeros)
+            {
+                listNumArrayList.Items.Add(num);
+            }
+
+            foreach (int num in vectorNumeros)
+            {
+                listNumVector.Items.Add(num);
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    listNumMatriz.Items.Add(matrizNumeros[i, j]);
+                }
+            }
+        }
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+            // Procesar los datos y mostrar resultados
+            try
+            {
+                int sumaLista = 0;
+                int sumaArray = 0;
+                int sumaVector = 0;
+                int sumaMatriz = 0;
+
+                //Generar una excepcion por intentantar acceder a un elemento en una posición que está fuera del rango.
+                //int numero = listaNumeros[10]; 
+
+                // Calcular la suma de elementos en cada estructura
+                foreach (int num in listaNumeros)
+                {
+                    sumaLista += num;
+                }
+
+                foreach (int num in arrayNumeros)
+                {
+                    sumaArray += num;
+                }
+
+                foreach (int num in vectorNumeros)
+                {
+                    sumaVector += num;
+                }
+
+                foreach (int num in matrizNumeros)
+                {
+                    sumaMatriz += num;
+                }
+
+                // Mostrar resultados en TextBoxes
+                txtSumaLista.Text = sumaLista.ToString();
+                txtSumaArray.Text = sumaArray.ToString();
+                txtSumaVector.Text = sumaVector.ToString();
+                txtSumaMatriz.Text = sumaMatriz.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al procesar datos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtSumaLista.Text = "";
+            txtSumaArray.Text = "";
+            txtSumaVector.Text = "";
+            txtSumaMatriz.Text = "";
+            listaNumeros.Clear();
+            arrayNumeros.Clear();
+            Array.Clear(vectorNumeros, 0, vectorNumeros.Length);
+            matrizNumeros = new int[3, 3];
+            listNumLista.Items.Clear();
+            listNumArrayList.Items.Clear();
+            listNumVector.Items.Clear();
+            listNumMatriz.Items.Clear();
+        }
+    }
+}
